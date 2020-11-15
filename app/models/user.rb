c3757f54c5e91d7892eecd9e66da50894a505d9d
sasaotomofumi <tomomasasso@gmail.com>
2020-11-16 08:27:11 +0900

    ユーザー管理機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'} do
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+  validates :first_name
+  validates :family_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+  validates :first_name_kana
+  validates :family_name_kana
+  end
+  validates :birth_day, presence: true
+
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
