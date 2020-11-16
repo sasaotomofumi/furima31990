70c7ed454336563cbdac27ba8cd5f4141eb7c7f8
sasaotomofumi <tomomasasso@gmail.com>
2020-11-16 10:32:09 +0900

    ユーザー管理機能実装　２回目

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_peemitted_parameters, if: :devise_controller?
-  
 
   private
-  def configure_peemitted_parameters 
+
+  def configure_peemitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day])
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,13 @@
-FactoryBot.define do 
-
-  factory :user do 
-      nickname { "furima" }
-      email {Faker::Internet.free_email}
-      password {Faker::Internet.password(min_length: 6)}
-      password_confirmation {password}
-      first_name { "太郎" }
-      family_name { "田中" }
-      first_name_kana { "タロウ" }
-      family_name_kana { "タナカ" }
-      birth_day  {Faker::Date.birthday(min_age:5, max_age: 90)} 
+FactoryBot.define do
+  factory :user do
+    nickname { 'furima' }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name { '太郎' }
+    family_name { '田中' }
+    first_name_kana { 'タロウ' }
+    family_name_kana { 'タナカ' }
+    birth_day { Faker::Date.birthday(min_age: 5, max_age: 90) }
   end
-
 end
